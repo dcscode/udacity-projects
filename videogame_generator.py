76650d573554b28997891651c8c7e99ff92ed8fd
dcscode <delaneycscode@gmail.com>
2021-04-13 23:33:28 -0400

    updated spacing

--- videogame_generator.py
+++ videogame_generator.py
@@ -12,19 +12,25 @@ def videogame_generator(genre, topic, title, templates):
   index = 0
   
   while index < len(template):
+    
     if template[index: index + 5] == "GENRE":
       output.append(random.choice(genre))
       index += 5
+      
     elif template[index: index + 5] == "TOPIC":
       output.append(random.choice(topic))
       index += 5
+      
     elif template[index: index + 5] == "TITLE":
       output.append(random.choice(title))
       index += 5
+      
     else:
       output.append(template[index])
       index += 1
-return "".join(output)
+      
+ return "".join(output)
+
 videogame_generator(videogame_info.genre, videogame_info.topic, videogame_info.title, videogame_info.templates)
 
 if __name__ == "__main__":
