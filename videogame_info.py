e5b0da9f73c6bdfb66c26c283abc453080185fd2
dcscode <delaneycscode@gmail.com>
2021-04-13 23:04:48 -0400

    Create videogame_info.py

---
+++ videogame_info.py
@@ -0,0 +1,18 @@
+# Data for videogame_generator function
+
+templates = ["TITLE TITLE, the next hit GENRE featuring TOPIC.",
+			"The latest GENRE, TITLE, TITLE, now with TOPIC.",
+			"A GENRE with TOPIC: TITLE TITLE.",
+			"TITLE TITLE: TOPIC GENRE!"]
+
+genre = ["FPS","RPG","roguelike","dunegeon-crawler","simulation game","platformer",
+		"battle-royale","MMO","MOBA", "strategy game","action game","adventure game",
+		"horror game","survival game"]
+
+topic = ["robots","AI","farming","sports","food","pets","space","time travel","plants",
+		"civilization","life","evolution","fashion","politics","hospital"]
+
+title = ["black","red","blue","white","high","low","neon","dark","mega","ultra",
+		"nano","iron","steel","sweet","swour","sword","space","sea","city","love",
+		"truth","tower","castle","underground","arena","shield","honor","cloud"",
+		"moon","sun","cat","dog","tiger","wolf","card"]
