c20395e696f0d268ff130d31818b3164ed9ade23
dcscode <delaneycscode@gmail.com>
2021-04-15 03:20:48 -0400

    restructured to take food and drink

--- food_bot/food_bot.py
+++ food_bot/food_bot.py
@@ -2,51 +2,44 @@
 
 import time
 
-options = ["breakfast","brunch","lunch","snack","dinner","dessert"]
+food = ["burritos","tacos","quesadilla","pizza","grilled cheese"]
 
-breakfast = ["eggs","toast","waffle","hashbrowns"]
+drinks = ["soda","horchata","water","tea"]
 
-lunch = ["sandwich","salad","tacos","burritos","pizza","soup"]
-
-mealtimes = [breakfast, lunch]
+answer = ["yes","no"]
 
 def print_timer(string):
 	print(string)
 	time.sleep(2)
 
-def valid_meal(prompt, options):
+def valid_order(prompt, order):
     while True:
-	    meal = input(prompt).lower()
-	    for option in options:
-		    if option in meal:
-			    return meal
+	    order = input(prompt).lower()
+	    for option in order:
+		    if option in order:
+			    return order
 	    print_timer("I do not understand.")
-    
-def valid_order(prompt, mealtimes):
-    while True:
-        order = input(prompt).lower()
-        for food in mealtimes:
-            if food in order
-                return order
-        print_timer("I do not understand.")
 
 def greeting():
 	print_timer("Hi, I'm Food Bot.")
 		
 def get_order():
-	meal = valid_meal("What meal are you having?\n", options)
-	if meal in options:
-		order = valid_order("What would you like for " + meal + "?\n", mealtimes)
-        if order in mealtimes:
-            print_timer("Your order has been placed.")
-	    else:
-		    print_timer("I do not understand.\n")
+	order = valid_order("What would you like to eat?\n", food)
+	if order in food:
+		drink = valid_order("Would you like a drink?\n", answer)
+     	if "no" in drink:
+			print_timer(order.capitalize() + ".")
+		if "yes" in drink:
+			drink = valid_order("What would you like to drink?\n", drinks)
+			print_timer(order.capitalize() + " and a " + drink + ".")
+       	print_timer("Your order has been placed.")
+	else:
+		print_timer("I do not understand.\n")
 	another_order()
 	
 	
 def another_order():
-	answer = ["yes","no"]
-	another_meal = valid_meal("Would you like to place another order?\n",answer)
+	another_meal = valid_order("Would you like to place another order?\n",answer)
 	if "no" in another_meal:
 		print_timer("Have a nice day!\n")
 	if "yes" in another_meal:
