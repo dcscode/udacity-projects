dbbbfef517d170530cfdc8eed4576fb56c9f7383
dcscode <delaneycscode@gmail.com>
2021-04-25 02:55:08 -0400

    Create photo_problem.py

---
+++ photo_problem/photo_problem.py
@@ -0,0 +1,25 @@
+import os
+
+def extract_place(filename):
+  return filename.split("_")[1]
+
+
+def make_place_directories(places):
+   for place in places:
+        os.mkdir(place)
+  
+        
+def organize_photos(directory):
+  os.chdir(directory)
+  originals = os.listdir()
+  places = []
+  for file in originals:
+      place = extract_place(file)
+      if place not in places:
+        places.append(place)
+  make_place_directories(places)
+  for file in orginals:
+    place = extract_place(file)
+    os.rename(file, 
+              os.path.join(place, file))
+    
