fcf70d674157be8ada0bb2c46f8674bcab5ae36c
dcscode <delaneycscode@gmail.com>
2021-04-29 08:35:56 -0400

    Create weather.py

---
+++ weather/weather.py
@@ -0,0 +1,14 @@
+import requests
+
+r = requests.get('https://www.metaweather.com/api/location/2442327')
+#print(r.text)
+d = r.json()
+print(d)
+
+print("Six day weather forecast for Louisville, KY")
+for forecast in d["consolidated_weather"]:
+    date = forecast["applicable_date"]
+    weather = forecast["weather_state_name"]
+    temp = forecast["the_temp"]
+    humidity = forecast["humidity"]
+    print(f"{date}\tWeather: {weather}\tTemperature: {temp}\tHumidity: {humidity}")
