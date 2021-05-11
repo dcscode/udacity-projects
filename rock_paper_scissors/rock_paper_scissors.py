9b4e3cfdc2b787083937415c6b4186cca081c775
dcscode <delaneycscode@gmail.com>
2021-05-11 05:01:08 -0400

    Create rock_paper_scissors.py

---
+++ rock_paper_scissors/rock_paper_scissors.py
@@ -0,0 +1,54 @@
+import random
+moves = ['rock', 'paper', 'scissors']
+
+#The Player class is the parent class for all of the Players
+in this game
+
+
+class Player:
+    def move(self):
+        return 'rock'
+
+    def learn(self, my_move, their_move):
+        pass
+
+            
+class RandomPlayer(Player):
+    def move(self):
+        return random.choice(moves)
+
+
+class Game:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+    
+    def beats(self, one, two):
+        return ((one == 'rock' and two == 'scissors') or
+                (one == 'scissors' and two == 'paper') or
+                (one == 'paper' and two == 'rock'))
+            
+    def play_round(self):
+        move1 = self.p1.move()
+        move2 = self.p2.move()
+        print(f"Player 1: {move1}  Player 2: {move2}")
+        self.p1.learn(move1, move2)
+        self.p2.learn(move2, move1)
+        if self.beats(move1, move2):
+            print("Player One Wins!")
+        elif self.beats(move2, move1):
+            print("Player Two Wins!")
+        else:
+            print("Tie!")
+        
+    def play_game(self):
+        print("Game start!")
+        for round in range(3):
+            print(f"Round {round}:")
+            self.play_round()
+        print("Game over!")
+
+
+if __name__ == '__main__':
+    game = Game(RandomPlayer(), RandomPlayer())
+    game.play_game()
